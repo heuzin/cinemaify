482755cd7e7d0bc44ea445e7ac8dda2ffe4650df
Matheus Silva <msilvaqs@outlook.com>
2024-10-11 21:41:27 -0500

    Changing secret

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -13,7 +13,7 @@ export const authOptions: NextAuthOptions = {
   jwt: {
     secret: process.env.NEXTAUTH_JWT_SECRET,
   },
-  secret: process.env.AUTH_SECRET,
+  secret: process.env.NEXT_PUBLIC_SECRET,
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID || "",
